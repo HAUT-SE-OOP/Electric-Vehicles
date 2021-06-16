44c00088310de3f3ebfd05fbcf69ab13533d046b
Jan Banka <jan.banka11@gmail.com>
2021-06-16 18:32:11 +0200

    added functionality: reading vehicles from file

--- ElectricVehicleStatus.h
+++ ElectricVehicleStatus.h
@@ -5,13 +5,15 @@ private:
     bool notCrashed;
     double price, mileage;
     int productionYear;
-    std::vector<ElectricVehicleStatus> vehicles;
-
-    ElectricVehicleStatus parser(const std::string& to_parse);
+    static ElectricVehicleStatus parser(const std::string& to_parse);
 public:
-    ElectricVehicleStatus();
+    ElectricVehicleStatus(const std::string& brand_ = "", const int hp_ = 0, const double range_ = 0, const std::string& originCountry_ = ""
+                          ,bool notCrashed_ = true, const double price_ = 0, const double mileage_ = 0, const int productionYear_ = 0)
+                          : ElectricVehicleParameters(brand_,hp_,range_),originCountry(originCountry_), notCrashed(notCrashed_), price(price_), mileage(mileage_)
+                          ,productionYear(productionYear_){}
 
-     void info() const override;
+    static std::vector<ElectricVehicleStatus> readFile(const std::string& filename);
+    void info() const override;
     ~ElectricVehicleStatus() = default;
 
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
 
+#include "ElectricVehicleStatus.h"
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    try {
+    //ElectricVehicleStatus ee = ElectricVehicleStatus::parser("BMW 120 250 Germany 1 150000 0 2019");
+    //ee.info();
+    std::vector<ElectricVehicleStatus> e = ElectricVehicleStatus::readFile("vehicles.txt");
+    for (int i = 0; i < e.size(); ++i) {
+        e[i].info();
+    }
+    }catch(std::runtime_error& error)
+    {
+        std::cout << error.what() << std::endl;
+    }
     return 0;
 }
