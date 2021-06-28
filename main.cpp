dcbbeabaa14f57b93a5f839492a72fa6942f8db1
Jan Bańka <68145213+janbanka11@users.noreply.github.com>
2021-06-28 13:42:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,7 @@
 #include "VehicleMenu.h"
 int main() {
     try {
-
-        ElectricVehicle e1{"AUDI",120,250,"Poland","true",15000,2300,2012};
-        ElectricVehicle e2{"BMW",110,300,"Germany","false",9000,197000,2005};
-        ElectricVehicle e3{"TESLA",230,450,"France","true",183000,0,2018};
+        
         VehicleMenu vm;
         vm.menu();
 
